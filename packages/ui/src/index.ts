0ff3c5e57ea8f82d198b7cdb51af4a6b460a93f1
Nathaniel Callahan <15992407+NDCallahan@users.noreply.github.com>
2026-07-22 16:24:25 -0500

    update property row actions to use shadcn dropdown-menu.

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -4,6 +4,7 @@ export * from "./components/button";
 export * from "./components/card";
 export * from "./components/checkbox";
 export * from "./components/drawer";
+export * from "./components/dropdown-menu";
 export * from "./components/input";
 export * from "./components/label";
 export * from "./components/logo";
